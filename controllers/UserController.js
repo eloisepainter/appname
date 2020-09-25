a565ef9b6693970aa23198491c6f136e91cd820f
eloise painter <eloise17226@avonside.school.nz>
2020-09-25 15:59:02 +1200

    channels

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,48 +11,44 @@
 class UserController{
 
     /**
-     * Instantiates class, calls method to set required session variables 
-     * for the user and assigns middleware to routes
+     * Instantiates class, calls method to set 
+     * required session variables 
+     * for the user
      */
     constructor(){
-
         // Set current user variables before each request
         this.setVariables();
+    }
 
+    /**
+     * Assigns middleware to add User to
+     * UserModel, request.session, 
+     * and response.locals variables
+     */
+    setVariables(){
+        AraDTApp.use(async (request, response, next) => {
+            // Pass on to next middleware
+            next();
+        });
+    }
+        
+    /**
+     * Assigns middleware to routes
+     */
+    addRoutes(){
         // Add all routing middleware for user endpoints
+        AraDTApp.get('/register', this.signup);
         AraDTApp.post('/register', this.register);
         AraDTApp.post('/login', this.login);
         AraDTApp.get('/logout', this.logout);
         AraDTApp.get('/account', this.getAccount);
         AraDTApp.post('/account', this.updateAccount);
         AraDTApp.post('/password', this.updatePassword);
-        AraDTApp.get('/login', this.getLogin);
-        AraDTApp.get('/register', this.getRegister);
     }
 
-    /**
-     * Assigns middleware to add Firebase.auth().currentUser to
-     * UserModel, request.session, and response.locals variables
-     */
-    setVariables(){
-        AraDTApp.use(async function(request, response, next) {
-            // Chesk if user logged in for this session
-            if (request.session.token) {
-                // We have a logged in user, so request user from Firebase
-                var currentUser = await AraDTDatabase.firebase.auth().currentUser;
-                if (currentUser != null) {
-                    // User returned, so add to session and local variables
-                    request.session.user = currentUser;
-                    response.locals.user = request.session.user;
-                    AraDTUserModel.setUser(currentUser);
-                    response.locals.loggedin = true;
-                    response.locals.user = currentUser;
-                }
-            }
-            // Pass on to next middleware
-            next();
-        });
-    }
+    signup = async (request, response) => {
+        response.render('register');
+    };
 
     /**
      * Asynchronous function that handles post form submission to '/login'
@@ -75,12 +71,12 @@ class UserController{
                 }).catch((error) => {
                     // Firebase login has failed, so return Firebase errors
                     request.session.errors.login = [error.message];
-                    response.redirect('/login');
+                    response.redirect('/');
                 });
         } catch(errors) {
             // Form has failed validation, so return errors
             request.session.errors.login = errors;
-            response.redirect('/login');
+            response.redirect('/');
         }
     };
 
@@ -103,16 +99,16 @@ class UserController{
             await AraDTUserModel.register(request, response)
                 .then(() => {
                     // registration successful, so redirect to account
-                    response.redirect('/');
+                    response.redirect('/account');
                 }).catch((error) => {
                     // Firebase registration has failed, so return Firebase errors
                     request.session.errors.register = [error.message];
-                    response.redirect('/');
+                    response.redirect('/register');
                 });
         } catch(errors) {
             // Form has failed validation, so return errors
             request.session.errors.register = errors;
-            response.redirect('/');
+            response.redirect('/register');
         }
     };
 
@@ -120,46 +116,18 @@ class UserController{
 
     updateAccount =  async (request, response) => {
 
-        var currentUser = AraDTUserModel.getCurrentUser();
-        if (currentUser) {
-            try{
-                await AraDTUserModel.update(request, response)
-                    .then(() => {
-                        response.locals.errors.profile = ['Your details have been updated'];
-                        response.render('account');
-                    }).catch((error) => {
-                        response.locals.errors.profile = [error.message];
-                        response.render('account');
-                    });
-            } catch(errors) {
-                response.locals.errors.profile = errors;
-                response.render('account');
-            }
-        } else {
-            this.logout(request, response);
-        }
-
-    };
-    
-    updatePassword = async (request, response) => {
-
-        var currentUser = AraDTUserModel.getCurrentUser();
-        if (currentUser) {
-            try{
-                await AraDTUserModel.updatePassword(request, response)
-                    .then(() => {
-                        response.locals.errors.password = ['Your password has been updated'];
-                        response.render('account');
-                    }).catch((error) => {
-                        response.locals.errors.password = [error.message];
-                        response.render('account');
-                    });
-            } catch(errors) {
-                response.locals.errors.password = errors;
-                response.render('account');
-            }
-        } else {
-            this.logout(request, response);
+        try{
+            await AraDTUserModel.update(request, response)
+                .then(() => {
+                    response.locals.errors.profile = ['Your details have been updated'];
+                    response.render('account');
+                }).catch((error) => {
+                    response.locals.errors.profile = [error.message];
+                    response.render('account');
+                });
+        } catch(errors) {
+            response.locals.errors.profile = errors;
+            response.render('account');
         }
 
     };
@@ -183,12 +151,37 @@ class UserController{
             });
     }
 
-    getLogin(request, response) {
-        response.render('login');
-    }
+    /**
+     * Asynchronous function that handles POST form submission to '/password'
+     * On success, redirects to '/account'
+     * Onfailure, redirects to '/account' with error message
+     * Requires the following POST form name fields:
+     * 
+     * @param {string}      request.body.password           password form field
+     * @param {string}      request.body.passwordConfirm    passwordConfirm form field
+     * 
+     * @returns {Object}    response.redirect object
+     */
+    updatePassword = async (request, response) => {
 
-    getRegister(request, response) {
-        response.render('register');
-    }
+        try{
+            await AraDTUserModel.updatePassword(request, response)
+                .then(() => {
+                    // updated password successful, so redirects to account
+                    request.session.errors.password = ['Your password has been updated'];
+                    response.redirect('/account');
+                }).catch((error) => {
+                    // updated password not succesfful, so keeps user on account and displays error
+                    request.session.errors.password = [error.message];
+                    response.redirect('/account');
+                });
+        } catch(errors) {
+            // Form has failed validation, so returns errors
+            request.session.errors.password = errors;
+            response.redirect('/account');
+        }
+
+    };
+
 }
 module.exports = UserController;
---
+++ models/ChannelModel.js
@@ -0,0 +1,352 @@
+/**
+ * ChannelModel Class.
+ * 
+ * Singleton class that wraps Firebase methods for 
+ * CRUD commands on channel docs
+ *
+ * @class
+ *
+ */
+class ChannelModel{
+
+
+    /**
+     * Model constructor. 
+     * 
+     * On subsequent instantiation, returns initial class.
+     */
+    constructor(){
+        
+        // If class already instatiated, return the initial instance
+        if (typeof this.instance === 'object') {
+            return this.instance;
+        }
+
+        // assign class instance to this.instance
+        this.instance = this;
+        return this;
+    }
+
+    /**
+     * readChannel method retrieves channel
+     * also fetches all users and separates
+     * members and non-members
+     * 
+     * @param {string} channelId channel to fetch from Firebase
+     * 
+     * @returns {Object} channel data
+     */
+    fetchChannel = async (channelId) => {
+
+        //Fetch channel and see if it exists
+        var channelDoc = AraDTDatabase.storage.collection('channels').doc(channelId);
+        var channel = {};
+        await channelDoc.get()
+            .then((datum) => {
+                if (!datum.exists) {
+                    //Channel does not exist
+                    throw new Error(['This channel does not exist']);
+                } else {
+                    //Get channel data
+                    channel = this.getChannelData(datum);
+                }
+            })
+            .catch((error) => {
+                throw error;
+            });
+
+        //Create initial arrays of users who belong, and users who do not
+        var inUsers = [];
+        var outUsers = [];
+        var hasUsers = !AraDTValidator.isEmptyObj(channel.users);
+        
+        //Loop through all users and assign
+        await AraDTUserModel.getUsers()
+            .then((data) => {
+                data.forEach((datum) => {
+                    if (channel.owner == datum.uid ||  
+                        (hasUsers && channel.users.includes(datum.uid))) {
+                        inUsers.push({
+                            id: datum.uid,
+                            name: datum.displayName,
+                            image: datum.photoURL,
+                        });
+                    } else {
+                        outUsers.push({
+                            id: datum.uid,
+                            name: datum.displayName,
+                            image: datum.photoURL,
+                        });
+                    }
+                });
+            })
+            .catch(function(error) {
+                throw new Error(error);
+            });
+
+        //return all user data
+        return{
+            channel,
+            inUsers,
+            outUsers
+        }
+    }
+    
+    /**
+     * readMessage method retrieves last 10 messages
+     * 
+     * @param {string} MessageId Message to fetch from Firebase
+     * 
+     * @returns {Object} Message data
+     */
+    fetchMessages = async (channelId, userId) => {
+
+        //Fetch message and see if it exists
+        var messages = [];
+        await AraDTDatabase.storage.collection('messages')
+            .where('channelId', '==', channelId)
+            .orderBy('time', 'desc')
+            .limit(10)
+            .get()
+            .then((data) => {
+                data.forEach((message) => {
+                    var message = message.data();
+                    message.direction = 'in'
+                    if (message.userId == userId) {
+                        message.direction = 'out'
+                    }
+                    messages.push(message);
+                });
+                if (messages.length == 0) {
+                    messages = false;
+                } else {
+                    messages.sort((a, b) => (a.time > b.time) ? 1 : -1);
+                }
+            })
+            .catch((error) => {
+                throw error;
+            });
+
+        return messages;
+    }
+
+    /**
+     * updateChannel method stores validated 
+     * channel data to Firebase
+     * 
+     * @param {Object} request Express request object
+     * @param {Object} response Express response object
+     * 
+     * @throws {Object} Error std error class
+     */
+    updateChannel = async (request, response) => {
+
+        var channelId = request.params.channelId;
+        
+        try {
+            //Build newly updated channel from request object
+            var updatedChannel = await this.buildChannel(request);
+    
+            //Update channel
+            var channelDoc = AraDTDatabase.storage.collection('channels').doc(channelId);
+            
+            await channelDoc.update(updatedChannel)
+                .catch((error) => {
+                    throw Error(error);
+                });
+
+        } catch(error) {
+            throw error;
+        }
+    }
+
+    /**
+     * addChannel method stores validated 
+     * channel data to Firebase as a new channel
+     * 
+     * @param {Object} request Express request object
+     * @param {Object} response Express response object
+     * 
+     * @throws {Object} Error std error class
+     */
+    addChannel = async (request, response) => {
+        
+        try {
+            //Build new channel from request object
+            var newChannel = await this.buildChannel(request);
+            //Update channel
+            await AraDTDatabase.storage.collection('channels')
+                .add(newChannel)
+                .catch((error) => {
+                    throw Error(error);
+                });
+
+        } catch(error) {
+            throw error;
+        }
+    }
+
+    /**
+     * buildChannel method fetches validated 
+     * channel data from POST request object
+     * 
+     * @param {Object} request Express request object
+     * 
+     * @throws {Object} Error std error class
+     */
+    buildChannel = async (request) => {
+        
+        var currentUser = request.session.user;
+        var slugName = AraDTValidator.makeSlug(request.body.name);
+        var image = '';
+        var avatar = (request.files && request.files.avatar) ? request.files.avatar : false;
+        var users = (request.body.users) ? request.body.users : [];
+        if (!Array.isArray(users)) {
+            users = [users];
+        }
+        
+        if (avatar) {
+            var { result, validExtension } = AraDTImageUpload.uploadImage(avatar, slugName);
+            if (validExtension) {
+                image = result;
+            } else {
+                throw Error(result);
+            }
+        } else if (request.body.avatar) {
+            image = request.body.avatar;
+        }
+
+        var channelData = {
+            owner: currentUser.uid,
+            name: request.body.name,
+            slug: slugName,
+            image: image,
+            users: users,
+            createdAt: new Date().toISOString()
+        }
+
+        return channelData;
+    }
+
+    /**
+     * channelId method deletes 
+     * channel doc from Firebase
+     * 
+     * @param {String} channelId 
+     * 
+     */
+    deleteChannel = async(channelId, currentUserId) => {
+
+        var channelDoc = AraDTDatabase.storage.collection('channels').doc(channelId);
+        await channelDoc.get()
+            .then((datum) => {
+                if (datum.data().owner == currentUserId) {
+                    channelDoc.delete();
+                } else {
+                    throw new Error(['You do not have authorisation to delete this channel']);
+                }
+            })
+            .catch(() => {
+                throw new Error(['This channel does not exist']);
+            });
+    }
+
+    /**
+     * Gets all channels where 
+     * the current user 
+     * is the owner
+     */
+    getOwnedChannels = async (request) => {
+
+        var currentUser = request.session.user;
+        var channels = [];
+        await AraDTDatabase.storage.collection('channels')
+            .where('owner', '==', currentUser.uid)
+            .orderBy('createdAt', 'desc')
+            .get()
+            .then((data) => {
+                data.forEach((datum) => {
+                    channels.push(this.getChannelData(datum));
+                });
+                if (channels.length == 0) {
+                    channels = false;
+                } else {
+                    channels.sort((a, b) => (a.users.length < b.users.length) ? 1 : -1);
+                }
+            })
+            .catch((error) => {
+                //Gracefully ignore query error
+                console.log('Error fetching channel data:' + error.message);
+            });
+        return channels;
+    }
+
+    /**
+     * Gets all channels where 
+     * the current user 
+     * is a member
+     */
+    getSubscribedChannels = async (request) => {
+
+        var currentUser = request.session.user;
+        var channels = [];
+
+        //Filter by channels containing user id
+        await AraDTDatabase.storage.collection('channels')
+            .where('users', 'array-contains', currentUser.uid)
+            .orderBy('createdAt', 'desc')
+            .get()
+            .then((data) => {
+                data.forEach((datum) => {
+                    channels.push(this.getChannelData(datum));
+                });
+                if (channels.length == 0) {
+                    channels = false;
+                } else {
+                    channels.sort((a, b) => (a.users.length < b.users.length) ? 1 : -1);
+                }
+            })
+            .catch((error) => {
+                //Gracefully ignore query error
+                console.log('Error fetching channel data:' + error.message);
+            });
+        return channels;
+    }
+
+    //Just fetches all channels
+    getAllChannels = async () => {
+
+        var channels = [];
+        await AraDTDatabase.storage.collection('channels')
+            .orderBy('createdAt', 'desc')
+            .get()
+            .then((data) => {
+                data.forEach((datum) => {
+                    channels.push(this.getChannelData(datum));
+                });
+                if (channels.length == 0) {
+                    channels = false;
+                } else {
+                    channels.sort((a, b) => (a.users.length < b.users.length) ? 1 : -1);
+                }
+            })
+            .catch((error) => {
+                console.log('Error fetching channel data:' + error.message);
+            });
+        return channels;
+    }
+
+    //Build channel data object from Firebase object
+    getChannelData(datum) {
+        return {
+            id: datum.id,
+            slug: datum.data().slug,
+            owner: datum.data().owner,
+            name: datum.data().name,
+            image: datum.data().image,
+            users: datum.data().users,
+            createdAt: datum.data().createdAt,
+        }
+    }
+}
+module.exports = ChannelModel;
